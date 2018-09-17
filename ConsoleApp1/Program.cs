c196ca5c788415286b336f56edacca4b6aed72cf
zzlProject <99105337@qq.com>
2018-09-17 23:14:22 +0800

    添加项目文件。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student stu = new Student();
+            //静态变量--->静态构造函数--->构造函数
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Student
+    {
+
+        public static string Name = "zzl";
+
+
+        static Student()
+        {
+
+        }
+
+        public Student()
+        {
+
+        }
+
+        public static string Sex = "男";
+    }
+}
